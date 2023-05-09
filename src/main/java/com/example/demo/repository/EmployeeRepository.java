f16529c9b5f70efdd236dd7fba80e0f663869192
Artem Kovalov <artemkov1990@gmail.com>
2023-05-09 11:45:57 +0300

    Receiving and closing the connection are moved to separate methods

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Employee;
-import jakarta.annotation.Nonnull;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -14,39 +13,25 @@ public final class EmployeeRepository {
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USER = "postgres";
     private static final String PASSWORD = "0";
+    private static Connection connection;
 
     private EmployeeRepository() {
     }
 
-    public static Connection getConnection() {
-
-        Connection connection = null;
-
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            if (connection != null) {
-                System.out.println("Connected to the PostgreSQL server successfully.");
-            } else {
-                System.out.println("Failed to make connection!");
-            }
-        } catch (SQLException sqlException) {
-            System.out.println(sqlException);
-        }
-        return connection;
-    }
-
     public static int save(Employee employee) {
         int status = 0;
-        try (Connection connection = getConnection();
-             PreparedStatement ps = connection.prepareStatement(INSERT_USER)) {
 
-            setNewEmployeeIntoDB(employee, ps);
+        getConnection();
+
+        try (PreparedStatement ps = connection.prepareStatement(INSERT_USER)) {
+            saveNewEmployeeIntoDB(employee, ps);
 
             status = ps.executeUpdate();
-
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
+
+        closeConnection();
         return status;
     }
 
@@ -54,17 +39,18 @@ public final class EmployeeRepository {
 
         int status = 0;
 
-        try (Connection connection = getConnection();
-             PreparedStatement ps = connection.prepareStatement(UPDATE_USER)) {
+        getConnection();
 
-            setNewEmployeeIntoDB(employee, ps);
+        try (PreparedStatement ps = connection.prepareStatement(UPDATE_USER)) {
+            saveNewEmployeeIntoDB(employee, ps);
             ps.setInt(4, employee.getId());
 
             status = ps.executeUpdate();
-
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         }
+
+        closeConnection();
         return status;
     }
 
@@ -72,41 +58,27 @@ public final class EmployeeRepository {
 
         int status = 0;
 
-        try (@Nonnull Connection connection = getConnection();
-             PreparedStatement ps = connection.prepareStatement(DELETE_USER)) {
+        getConnection();
+
+        try (PreparedStatement ps = connection.prepareStatement(DELETE_USER)) {
             ps.setInt(1, id);
 
             status = ps.executeUpdate();
-
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
+
+        closeConnection();
         return status;
-    }
-
-    public static Employee getEmployeeById(int id) {
-
-        Employee employee = null;
-
-        try (Connection connection = getConnection();
-             PreparedStatement ps = connection.prepareStatement(SELECT_BY_ID)) {
-            ps.setInt(1, id);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                employee = getNewEmployee(rs);
-            }
-        } catch (SQLException exception) {
-            exception.printStackTrace();
-        }
-        return employee;
     }
 
     public static List<Employee> getAllEmployees() {
 
         List<Employee> listEmployees = new ArrayList<>();
 
-        try (Connection connection = getConnection();
-             PreparedStatement ps = connection.prepareStatement(SELECT_ALL)) {
+        getConnection();
+
+        try (PreparedStatement ps = connection.prepareStatement(SELECT_ALL)) {
             ResultSet rs = ps.executeQuery();
 
             while (rs.next()) {
@@ -114,11 +86,53 @@ public final class EmployeeRepository {
 
                 listEmployees.add(employee);
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        closeConnection();
         return listEmployees;
+    }
+
+    public static Employee getEmployeeById(int id) {
+
+        Employee employee = null;
+
+        getConnection();
+        try (PreparedStatement ps = connection.prepareStatement(SELECT_BY_ID)) {
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                employee = getNewEmployee(rs);
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+
+        closeConnection();
+        return employee;
+    }
+
+    private static void getConnection() {
+
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException sqlException) {
+            System.out.println("Failed to make connection!");
+            sqlException.printStackTrace();
+        }
+    }
+
+    private static void closeConnection() {
+        try {
+            if (!connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     private static Employee getNewEmployee(ResultSet resultSet) throws SQLException {
@@ -131,7 +145,7 @@ public final class EmployeeRepository {
         return employee;
     }
 
-    private static void setNewEmployeeIntoDB(Employee employee, PreparedStatement ps) throws SQLException {
+    private static void saveNewEmployeeIntoDB(Employee employee, PreparedStatement ps) throws SQLException {
         ps.setString(1, employee.getName());
         ps.setString(2, employee.getEmail());
         ps.setString(3, employee.getCountry());
